173b275cef64fcf475d3cf60835b65e6ff5222bc
salman <salmanirfan674@gmail.com>
2022-10-03 17:58:51 +0500

    2.1.20 lab variables

---
+++ chapter2/2.1/2.1.1.20LabVariables.js
@@ -0,0 +1,25 @@
+// Scenario
+// Our task will be to create a list of contacts. Initially, the list will be quite simple: we will only write three people to it using the data shown in the table below. In the rest of the course, you will return to this script and systematically extend it with new functionality, using the newly learned elements of JavaScript.
+
+// Name	                Phone	            Email
+// Maxwell Wright	    (0191) 719 6495	    Curabitur.egestas.nunc@nonummyac.co.uk
+// Raja Villarreal	    0866 398 2895	    posuere.vulputate@sed.com
+// Helen Richards	    0800 1111	        libero@convallis.edu
+
+const header = ["Name", "Phone", "Email"];
+const desc1 = ["Maxwell Wright", "(0191) 719 6495", "Curabitur.egestas.nunc@nonummyac.co.uk"];
+const desc2 = ["Raja Villarreal", "0866 398 2895", "posuere.vulputate@sed.com"];
+const desc3 = ["Helen Richards", "0800 1111", "libero@convallis.edu"];
+
+console.log(header)
+console.log(desc1)
+console.log(desc2)
+console.log(desc3)
+
+const names = ["Maxwell Wright", "Raja Villarreal", "Helen Richards"];
+const phones = ["(0191) 719 6495", "0866 398 2895", "0800 1111"];
+const emails = ["Curabitur.egestas.nunc@nonummyac.co.uk",  "posuere.vulputate@sed.com", "libero@convallis.edu" ];
+
+console.log(names, "\n")
+console.log(phones, "\n")
+console.log(emails, "\n")
