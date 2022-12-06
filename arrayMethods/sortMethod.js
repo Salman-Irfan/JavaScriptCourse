5123bc557aafcf2abc631637040fc1138fad97c0
salman <salmanirfan674@gmail.com>
2022-12-06 15:18:32 +0500

    sort method for array

---
+++ arrayMethods/sortMethod.js
@@ -0,0 +1,8 @@
+const numbers = [,1100,1300, 1200,400,300]
+numbers.sort()
+console.log(numbers)
+//numbers ko as a string treat kry ga, as ASCII
+
+const names = ["salman", "abcd", "xyz"]
+names.sort()
+console.log(names)
