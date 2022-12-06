7318eb537bfcf1d3724a9849d9ad37e69c1d79ef
salman <salmanirfan674@gmail.com>
2022-12-06 18:53:07 +0500

    splice method of array

---
+++ arrayMethods/spliceMethod.js
@@ -0,0 +1,6 @@
+const myArray = [1,2,3,4,5];
+const a = myArray.splice(1,2,"one")
+// splice(1,2,"one")
+// splice(index at which new insertion, number after how many will be deleted, insert array)
+console.log(myArray)
+console.log(a)
