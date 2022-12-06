ac55c11cbca651fb34a85260988b13122b063eaf
salman <salmanirfan674@gmail.com>
2022-12-06 19:36:01 +0500

    sets

--- sets/sets.js
+++ sets/sets.js
@@ -1,2 +1,18 @@
-const number = new Set([1,2,3,2]);
-console.log(number)
+//sets 
+// it is iterable
+// store data
+// sets also have its methods
+// no index based access
+// unique items only (no duplicates are allowed)
+
+const numbers = new Set([1,2,3,2]);
+console.log(numbers)
+if(numbers.has(1)){
+    console.log("1 is present")
+}
+else{
+    console.log("1 is not present")
+}
+for(let number of numbers){
+    console.log(number)
+}
