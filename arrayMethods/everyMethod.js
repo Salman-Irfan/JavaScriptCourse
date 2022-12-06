873730e620e7742b1cd9cdba61d282c6de5ff60f
salman <salmanirfan674@gmail.com>
2022-12-06 16:51:34 +0500

    every method of array

--- arrayMethods/everyMethod.js
+++ arrayMethods/everyMethod.js
@@ -4,3 +4,12 @@ function isEven (number){
 }
 const ans = numbers.every(isEven);
 console.log(ans)
+// real problem
+// tell all the products are less than 40,000 or not
+const userCart = [
+    {pid: 1, pname: "mobile", price: 12000},
+    {pid: 2, pname: "laptop", price: 22000},
+    {pid: 3, pname: "led", price: 32000},
+]
+const ansPrice = userCart.every((cartItem)=>cartItem.price < 40000)
+console.log(ansPrice)
