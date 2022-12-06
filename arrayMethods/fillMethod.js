c0ee7e5c15941780f26d3dceabc23c11071b6a69
salman <salmanirfan674@gmail.com>
2022-12-06 18:39:20 +0500

    fiil method of array

---
+++ arrayMethods/fillMethod.js
@@ -0,0 +1,7 @@
+const myArray = new Array(10).fill(-3);
+console.log(myArray)
+const myArray2 = [1,2,3,4,5,6,7,8];
+myArray2.fill(0,2,5)
+console.log(myArray2)
+// fill(0,2,5)
+// fill (number to be filled, start index, end index)
