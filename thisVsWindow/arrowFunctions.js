ec328d8af4b47211fb05fc54b98854c56df4c01e
salman <salmanirfan674@gmail.com>
2023-01-06 15:50:15 +0500

    this os arraow functions

---
+++ thisVsWindow/arrowFunctions.js
@@ -0,0 +1,10 @@
+// arrow function does not has its own "this".It takes "this" from its surrounding (means one level up). nd we can not change "this" f arrow function. For example by using "call()", "this" will not change. 
+const user1 = {
+    firstName: "salman",
+    age: 20,
+    about: () => {
+        console.log(this.firstName, this.age) //in this case "this" is one level up i.e. "window"
+    }
+}
+user1.about() //undefined undefined
+user1.about.call(user1) //undefined undefined
