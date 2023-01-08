eb62dd08c8fc093a47970e7cc4ef273773cf8113
salman <salmanirfan674@gmail.com>
2023-01-08 13:57:43 +0500

    inheritance

---
+++ oop/classes/inheritance.js
@@ -0,0 +1,30 @@
+class Animal {
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+    eat(){
+        return `${this.name} is eating`; 
+    }
+    isSuperCute(){
+        return this.age < 1;
+    }
+    isCute(){
+        return true;
+    }
+}
+const animal1 = new Animal("tom", 2)
+console.log("animal1 = ", animal1)
+console.log("animal1.eat() = ", animal1.eat())
+console.log("animal1.isCute() = ", animal1.isCute())
+console.log("animal1.isSuperCute() = ", animal1.isSuperCute())
+
+// dog class
+class Dog extends Animal {
+    
+}
+const tommy = new Dog("tommy", 3)
+console.log("tommy = ", tommy)
+console.log("tommy.eat() = ", tommy.eat())
+console.log("tommy.isCute() = ", tommy.isCute())
+console.log("tommy.isSuperCute() = ", tommy.isSuperCute())
