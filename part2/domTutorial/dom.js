50065c948bb3bf55cbec3cca2c85c91ea23aae9d
salman <salmanirfan674@gmail.com>
2023-01-12 12:54:41 +0500

    dom started

---
+++ part2/domTutorial/dom.js
@@ -0,0 +1,18 @@
+// DOM
+// Document Object Model
+// overview
+// how to use
+// deep study
+
+// browser gets an HTML file
+// HTML
+// head => 1. title, 2. meta
+// body => i. header, ii. section
+// body.header => a. nav, b. logo
+
+// JS will make this an object, we call this object as document. and assign this document to window object
+
+console.log(window.document)
+console.dir(window.document)
+console.log(document)
+console.dir(document)
