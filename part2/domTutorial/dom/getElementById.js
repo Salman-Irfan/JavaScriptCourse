a44536983b594349bcefef21c8d320c237a144a5
salman <salmanirfan674@gmail.com>
2023-01-12 13:23:40 +0500

    select element using getElementByID and getElementByClassName

---
+++ part2/domTutorial/dom/getElementById.js
@@ -0,0 +1,11 @@
+console.log(document.getElementById("main-heading"))
+console.dir(document.getElementById("main-heading"))
+console.log(typeof document.getElementById("main-heading"))
+console.log(document.getElementsByClassName("headline"))
+console.dir(document.getElementsByClassName("headline"))
+console.log(document.getElementById("test"))
+console.log(document.getElementsByClassName("logo"))
+console.dir(document.getElementsByClassName("logo"))
+
+const logo = document.getElementsByClassName("logo")
+console.log("logo = ", logo)
