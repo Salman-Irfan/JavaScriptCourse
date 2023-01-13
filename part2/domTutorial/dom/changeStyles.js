67d6422f755dcc7ebc14782ca52fd1534d9b88cc
salman <salmanirfan674@gmail.com>
2023-01-13 10:33:41 +0500

    change styles of elements

---
+++ part2/domTutorial/dom/changeStyles.js
@@ -0,0 +1,13 @@
+console.log("change styles")
+
+//  change the styles of elements
+
+const styleHeading = document.querySelector("div.headline h2")
+
+console.log("styleHeading = ", styleHeading)
+
+console.log("styleHeading.style = ", styleHeading.style)
+
+styleHeading.style.backgroundColor = "black"
+styleHeading.style.color = "yellow"
+styleHeading.style.border = "2px dashed green"
