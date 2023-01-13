3a606783efb1cecafc7dc9ee780262c713725014
salman <salmanirfan674@gmail.com>
2023-01-13 18:15:54 +0500

    for loop on HTML collection => getElementsByClassName

---
+++ part2/domTutorial/dom/forLoop.js
@@ -0,0 +1,30 @@
+console.log("for Loop")
+
+// array like object => indexing, length property
+let forNavItems = document.getElementsByTagName("a")
+console.log("forNavItems = ", forNavItems )
+console.log("forNavItems.length = ", forNavItems.length)
+
+// we cant use for each loop in html collection
+// simple for loop
+// for of loop
+// for each
+
+// for (let i=0; i < ForNavItems.length; i++){
+//     console.log(ForNavItems[i])
+//     const navItem = navItems[i];
+//     navItem.style.backgroundColor = "black"
+//     navItem.style.fontWeight = "bold"
+// }
+
+// for (let forNavItem of forNavItems){
+//     forNavItem.style.backgroundColor = "black"
+//     forNavItem.style.fontWeight = "bold"
+// }
+
+forNavItems = Array.from(forNavItems);
+console.log("Array.isArray(forNavItems) = ", Array.isArray(forNavItems))
+forNavItems.forEach((forNavItem) => {
+    forNavItem.style.backgroundColor = "black"
+    forNavItem.style.fontWeight = "bold"
+});
