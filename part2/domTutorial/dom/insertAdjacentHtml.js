f4ac9480e93adc086be2eed9e3fe1287db7539ce
salman <salmanirfan674@gmail.com>
2023-01-14 17:12:20 +0500

    insert adjacent html

---
+++ part2/domTutorial/dom/insertAdjacentHtml.js
@@ -0,0 +1,8 @@
+// element.insertAdjacentHTML(where, html)
+// beforeBegin
+// afterBegin
+// beforeEnd
+// afterEnd
+
+// const todoList = document.querySelector(".todo-list")
+// todoList.insertAdjacentHTML("afterend", "<li>any string </li>")
