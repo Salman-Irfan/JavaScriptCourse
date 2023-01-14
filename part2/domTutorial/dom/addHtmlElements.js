e38989d8459073be01abbd074bf5546ee2d5c0f4
salman <salmanirfan674@gmail.com>
2023-01-14 15:53:24 +0500

    add new elements in innerHtml

---
+++ part2/domTutorial/dom/addHtmlElements.js
@@ -0,0 +1,17 @@
+console.log("add html elements")
+
+// add new elementd to the page
+// innerHtml to html element
+// when you should usr it, when yoi shoulf not use it
+
+const todoList = document.querySelector(".todo-list")
+console.log("todoList = ", todoList)
+console.log("todoList.innerHTML = ", todoList.innerHTML)
+
+todoList.innerHTML += "<li>New Todo</li>"
+
+// when you should usr it, when yoi shoulf not use it
+// performance issues => avoid using it
+
+// when to use it? 
+// when we have to change the inner HTML, not to add in existing html
