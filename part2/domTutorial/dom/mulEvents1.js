80c9c8ad05c83ecd3556f475189eaa56e8121970
salman <salmanirfan674@gmail.com>
2023-01-15 17:46:41 +0500

    multiple events

--- part2/domTutorial/dom/mulEvents1.js
+++ part2/domTutorial/dom/mulEvents1.js
@@ -1,9 +1,9 @@
 console.log("mul events 1")
 
-// const firstButton = document.querySelector("#one")
-// firstButton.addEventListener("click", function(){
-//     console.log("you clicked button one")
-// })
+const firstButton = document.querySelector("#one")
+firstButton.addEventListener("click", function(){
+    console.log("you clicked button one")
+})
 
 const allButtons = document.querySelectorAll("button")
 console.log(allButtons)
