0cc36a2baada184c22b8ff9484fb49b2a6d8a463
salman <salmanirfan674@gmail.com>
2023-01-15 20:41:47 +0500

    practice of event object

---
+++ part2/domTutorial/dom/practiceEventObject.js
@@ -0,0 +1,12 @@
+console.log("practice event object")
+// change the style 
+const allButtons = document.querySelectorAll(".my-buttons button")
+// console.log(allButtons.length)
+
+allButtons.forEach((button)=>{
+    button.addEventListener("click", (e)=>{
+        // console.log(e.target)
+        e.target.style.backgroundColor = "yellow"
+        e.target.style.color = "red"
+    })
+})
