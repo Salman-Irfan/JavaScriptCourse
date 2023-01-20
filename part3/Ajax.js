8bf085bbcda30285e646e393fec4c524e86d5706
salman <salmanirfan674@gmail.com>
2023-01-20 07:53:04 +0500

    ajax

---
+++ part3/Ajax.js
@@ -0,0 +1,93 @@
+const URL = "https://jsonplaceholder.typicode.com/posts";
+const xhr = new XMLHttpRequest();
+console.log(xhr)
+
+// step 1
+console.log("xhr = ", xhr);
+console.log("xhr.readyState = ", xhr.readyState);
+
+xhr.open("GET", URL);
+
+console.log("xhr = ", xhr);
+console.log("xhr.readyState = ", xhr.readyState);
+
+xhr.onreadystatechange = function() {
+    console.log("xhr = ", xhr);
+    console.log("xhr.readyState = ", xhr.readyState);
+
+    if(xhr.readyState == 4){
+        const response = xhr.response;
+        console.log(response);
+        console.log(typeof response); // string
+        const data = JSON.parse(response);
+        console.log(data);
+        console.log(typeof data); // object
+    }
+}
+
+xhr.onload = function(){
+    console.log("onload function will only run if the xhr.readyState = 4")
+    console.log(xhr.readyState)
+}
+xhr.send();
+//////////////////////////////////////////
+
+// basic theory
+
+// AJAX: asynchronous java script and XML
+
+// HTTP request
+
+// is a set of "web development techniques"
+// using many web technologies on the "client side"
+// to create asynchronous web applications
+
+// with Ajax, web aoolications can send and retrieve
+// data from a server asynchronously (in the background)
+// without interfering with the display and
+// behavior of the existing page
+
+// we don't use data in XML foemat anymore
+// we use JSON now
+
+// we have 3 most common ways to create ways to create and request yo server
+
+// 1. xmlHttpRequest (old way of doing)
+// 2. Fetch API (new way of doing)
+// 3. axios (this is third party library)
+
+
+// important 
+// status
+// readyState
+// response
+
+// Search => json TypIcode
+// rest api stands for => representational state transfer
+
+
+// Routes
+// All HTTP methods are supported. You can use http or https for your requests.
+
+// GET	/posts              => get / read soma data 
+// GET	/posts/1           
+// GET	/posts/1/comments               
+// GET	/comments?postId=1              
+// POST	/posts              => create some data
+// PUT	/posts/1            => change / update in existing data    
+// PATCH	/posts/1        => minor changing in existing data        
+// DELETE	/posts/1        => delete some data        
+
+
+// Resources
+// JSONPlaceholder comes with a set of 6 common resources:
+
+// /posts	100 posts
+// /comments	500 comments
+// /albums	100 albums
+// /photos	5000 photos
+// /todos	200 todos
+// /users	10 users
+
+// browser by default does "GET" request to server
+
